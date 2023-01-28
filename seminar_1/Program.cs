a0b843bc7f6a16909cff8f5771dba72760321378
arrivvaa <vv.var.vfx@gmail.com>
2023-01-28 23:14:09 +0300

    feat: correct

--- seminar_1/Program.cs
+++ seminar_1/Program.cs
@@ -5,12 +5,12 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-// Console.WriteLine("Введите первое число: ");
-// int a = int.Parse(Console.ReadLine());
-// Console.WriteLine("Введите второе число: ");
-// int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
 
-// Console.WriteLine($"max = {Math.Max(a,b)}");
+Console.WriteLine($"max = {Math.Max(a,b)}");
 //=============================================================================
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
@@ -19,11 +19,11 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-// Console.WriteLine("Введите числа через пробел: ");
-// string[] string_number = Console.ReadLine().Split();
-// int[] int_numbers = Array.ConvertAll(string_number, s => int.Parse(s));
+Console.WriteLine("Введите числа через пробел: ");
+string[] string_number = Console.ReadLine().Split();
+int[] int_numbers = Array.ConvertAll(string_number, s => int.Parse(s));
 
-// Console.WriteLine($"max = {int_numbers.Max()}");
+Console.WriteLine($"max = {int_numbers.Max()}");
 //=============================================================================
 
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
@@ -34,16 +34,16 @@
 // 7 -> нет
 
 
-// Console.WriteLine("Введите число: ");
-// int a = int.Parse(Console.ReadLine());
-// if (a % 2 != 0)
-// {
-//     Console.WriteLine("нет");
-// }
-// else
-// {
-//     Console.WriteLine("да");
-// }
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 != 0)
+{
+    Console.WriteLine("нет");
+}
+else
+{
+    Console.WriteLine("да");
+}
 //=============================================================================
 
 // Задача 8: Напишите программу, которая на вход принимает число (N), 
@@ -52,12 +52,12 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-// Console.WriteLine("Введите число: ");
-// int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
 
-// for (int i = 1; i <= a; i++){
-//     if (i % 2 == 0){
-//         Console.Write($"{i} ");
-//     }
-// }
+for (int i = 1; i <= a; i++){
+    if (i % 2 == 0){
+        Console.Write($"{i} ");
+    }
+}
 
