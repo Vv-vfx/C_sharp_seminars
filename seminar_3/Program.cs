fd0ce1589b4750f953603cb10942526f3920840f
arrivvaa <vv.var.vfx@gmail.com>
2023-02-10 21:40:54 +0300

    feat: sem_3

---
+++ seminar_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+string a = Console.ReadLine();
+char[] arr = a.ToCharArray();
+Array.Reverse(arr);
+string new_a = new string(arr);   //здесь вопрос
+if (a == new_a) System.Console.WriteLine("Полиндром");
+else System.Console.WriteLine("Не Полиндром");
+
+
+
+
+
