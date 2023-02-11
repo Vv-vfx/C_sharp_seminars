a6a70658e5f2fc2de9bf73805f73a2aa1cd6f0c3
arrivvaa <vv.var.vfx@gmail.com>
2023-02-11 18:00:58 +0300

    feat: new Seminar 4

--- seminar_4/Program.cs
+++ seminar_4/Program.cs
@@ -3,17 +3,17 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-// Console.WriteLine("Введите число: ");
-// int number = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите степень: ");
-// int degree  = Convert.ToInt32(Console.ReadLine());
-// int result = 1;
-// for (int i = 1; i <= degree; i++)
-// {   
-//     result *= number;
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int degree  = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 1; i <= degree; i++)
+{   
+    result *= number;
 
-// }
-// System.Console.WriteLine($"{number}  в степени {degree} = {result}");
+}
+System.Console.WriteLine($"{number}  в степени {degree} = {result}");
 
 // =====================================================
 // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
@@ -22,10 +22,10 @@
 // 9012 -> 12
 
 
-// Console.WriteLine("Введите число: ");
-// char[] string_number = Console.ReadLine().ToCharArray();
-// int[] int_number = Array.ConvertAll(string_number, s => (int)Char.GetNumericValue(s));
-// System.Console.WriteLine(int_number.Sum());
+Console.WriteLine("Введите число: ");
+char[] string_number = Console.ReadLine().ToCharArray();
+int[] int_number = Array.ConvertAll(string_number, s => (int)Char.GetNumericValue(s));
+System.Console.WriteLine(int_number.Sum());
 
 // =====================================================
 
@@ -36,11 +36,7 @@
 Console.WriteLine("Введите числа через пробел: ");
 int[] string_number = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
 System.Console.WriteLine(string.Join(',', string_number));
-System.Console.WriteLine($"string_number type in {string_number.GetType()}");
-
-// int[] int_number = Array.ConvertAll(string_number, s => (int)Char.GetNumericValue(s));
-// System.Console.WriteLine(int_number.Sum());
-
+System.Console.WriteLine($"string_number type in {string_number.GetType()}");`
 
 
 
